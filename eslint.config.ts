506354cd9e606b8f344e4b148e10cac223161c58
Jack Herby <JacekZiolek@proton.me>
2025-03-19 20:33:52 +0100

    Add Stylistic to ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,8 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginPlaywright from 'eslint-plugin-playwright'
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVitest from '@vitest/eslint-plugin';
+import pluginPlaywright from 'eslint-plugin-playwright';
+import stylistic from '@stylistic/eslint-plugin';
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -21,14 +22,23 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
+  {
+    ...stylistic.configs.recommended,
+    rules: {
+      ...stylistic.configs.recommended.rules,
+      '@stylistic/semi': 'error',
+      'semi': ['error', 'always'],
+    },
+  },
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
-)
+);
