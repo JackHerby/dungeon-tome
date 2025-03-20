09d4b787247686170c7418498f5ca95e7583d5b2
Jack Herby <JacekZiolek@proton.me>
2025-03-20 22:44:59 +0100

    Add PrimeVue UI library

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Button
+      label="PrimeVue Button"
+      severity="danger"
+      size="large"
+    />
   </div>
 </template>
 
@@ -9,6 +18,8 @@
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
   }
 }
